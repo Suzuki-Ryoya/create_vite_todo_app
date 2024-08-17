7b2024ed3eca44cd630f7e3f0901eae41e93c2d1
Ryoya Suzuki <ryoya1206sr@icloud.com>
2024-08-17 21:35:22 +0900

    create my todo-app

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -4,10 +4,12 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     VitePWA({
       manifest: {
+        id: "/todo/",
         name: "Todo App (PWA)",
         short_name: "Todo",
         description: "Todo プログレッシブ・ウェブアプリ",
